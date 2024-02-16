3f8fcda8231ee893b9f737c9d27a972014e2cf82
Daniela Tenório <danidaniela.tenorio@gmail.com>
2024-02-16 18:40:15 +0000

    Alteraç]oes no html e js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
-let formElement = document.querySelector(".popup__form");
-let popupClose = document.querySelector(".popup__btn-close");
-let btnEdit = document.querySelector(".profile__btn-edit");
+const formElement = document.querySelector(".popup__form");
+const popupClose = document.querySelector(".popup__btn-close");
+const btnEdit = document.querySelector(".profile__btn-edit");
 
-let nameInput = formElement.querySelector(".popup__input_type_name");
-let jobInput = formElement.querySelector(".popup__input_type_about");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_about");
 
-let nameText = document.querySelector(".profile__name");
-let jobText = document.querySelector(".profile__about");
+const nameText = document.querySelector(".profile__name");
+const jobText = document.querySelector(".profile__about");
 
 function togglePopup() {
-  let popup = document.querySelector(".popup");
+  const popup = document.querySelector(".popup");
   popup.classList.toggle("popup_opened");
   nameInput.value = nameText.textContent;
   jobInput.value = jobText.textContent;
@@ -27,18 +27,18 @@ popupClose.addEventListener("click", togglePopup);
 btnEdit.addEventListener("click", togglePopup);
 
 // Add event listener to each like button
-let likeButtons = document.querySelectorAll(".posts__btn-like");
+const likeButtons = document.querySelectorAll(".posts__btn-like");
 likeButtons.forEach(function (button) {
   button.addEventListener("click", function () {
     button.classList.toggle("posts__btn-like_actived");
   });
 });
 
-let trashButtons = document.querySelectorAll(".posts__trash-btn");
+const trashButtons = document.querySelectorAll(".posts__trash-btn");
 trashButtons.forEach(function (trashButton) {
   trashButton.addEventListener("click", function () {
     // Encontre o elemento pai (li) do botão de lixeira
-    let postElement = trashButton.closest(".posts__item");
+    const postElement = trashButton.closest(".posts__item");
 
     // Verifique se o elemento foi encontrado antes de tentar removê-lo
     if (postElement) {
